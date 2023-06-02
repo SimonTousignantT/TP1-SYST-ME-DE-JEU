adb5efeb68e93a4e2f8319b8c73ab4afcfaf6104
simon tousignant.t <2230152@bart.ca>
2023-06-02 14:06:24 -0400

    les dialogue son gerer par des scriptable object et un docu text est placer dans les dossier pour expliquer les commande

---
+++ TP1System de jeu/Assets/tousignant.t simon/Script/ScriptableObject/PortalLvl/TextLorObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "DataPortalText", menuName = "ScriptableObjects/TextPortal", order = 1)]
+public class TextLorObject : ScriptableObject
+{
+    // Start is called before the first frame update
+    public string[] m_textLvl = { " ", " ", " ", " " };
+  
+}
